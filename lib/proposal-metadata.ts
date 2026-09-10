d07cf8c086e2f7e02b67967c81ea685823691437
Rolando Valle <rolandovalle0428@gmail.com>
2026-09-10 20:34:52 +0800

    Update

--- lib/proposal-metadata.ts
+++ lib/proposal-metadata.ts
@@ -3,10 +3,10 @@ import { siteConfig } from "@/content/site"
 
 const siteUrl =
   process.env.NEXT_PUBLIC_SITE_URL ??
-  "https://jen-and-tin-wedding.weddinginvitationrsvp.com/"
+  "https://siddhesh-and-jennifer.weddinginvitationrsvp.com/"
 const canonicalBase = siteUrl.replace(/\/$/, "")
 
-export const PROPOSAL_OG_IMAGE_PATH = "/Details/LinkPreview.png"
+export const PROPOSAL_OG_IMAGE_PATH = "/Details/linkPreview.png"
 export const PROPOSAL_OG_IMAGE_URL = `${canonicalBase}${PROPOSAL_OG_IMAGE_PATH}`
 
 const coupleNames = `${siteConfig.couple.groomNickname} & ${siteConfig.couple.brideNickname}`
